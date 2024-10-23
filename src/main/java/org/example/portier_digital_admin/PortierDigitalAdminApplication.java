55f20fc36a2225045cc0cdc834d871154b4b69a9
Zidan4ik <roman.pravnyk.tr.2021@lpnu.ua>
2024-10-23 12:46:13 +0300

    Initialize

---
+++ src/main/java/org/example/portier_digital_admin/PortierDigitalAdminApplication.java
@@ -0,0 +1,13 @@
+package org.example.portier_digital_admin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PortierDigitalAdminApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PortierDigitalAdminApplication.class, args);
+	}
+
+}
