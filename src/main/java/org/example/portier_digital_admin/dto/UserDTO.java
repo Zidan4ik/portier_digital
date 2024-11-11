6d47092321b43acdffd1cda5463a2abc9af1d495
Zidan4ik <roman.pravnyk.tr.2021@lpnu.ua>
2024-11-11 15:30:24 +0200

    Fix functional with authority

--- src/main/java/org/example/portier_digital_admin/dto/UserDTO.java
+++ src/main/java/org/example/portier_digital_admin/dto/UserDTO.java
@@ -1,6 +1,7 @@
 package org.example.portier_digital_admin.dto;
 
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -14,6 +15,7 @@ public class UserDTO {
     @NotBlank(message = "{error.field.empty}")
     private String firstName;
     @NotBlank(message = "{error.field.empty}")
+    @Pattern(regexp = "([A-z0-9_.-]{1,})@([A-z0-9_.-]{1,}).([A-z]{2,8})",message = "{error.field.email.not.valid}")
     private String email;
     @Size(min = 4,max = 30,message = "@{error.field.size.between}")
     private String password;
